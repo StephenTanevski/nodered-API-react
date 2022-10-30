6ae22928556884996fc80f4f68aee3dc2e8e0731
traptrap9 <johnnnamos@gmail.com>
2022-10-30 16:30:50 +1000

    Reconfigured db

--- iab330/db.js
+++ iab330/db.js
@@ -3,7 +3,7 @@ const {Client} = require('pg')
 const client = new Client({
     user: "postgres",
     host: "localhost",
-    database: "sequelize_db",
+    database: "iab330nodered",
     password: "postgres",
     port: 5432,
 })
